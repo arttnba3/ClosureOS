9186f39ff8d2c44ddbbab6307ddbb5d7d351be08
arttnba3 <arttnba3@outlook.com>
2024-07-03 03:17:17 +1000

    add return value for global constructor caller

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,18 +5,24 @@
 extern void boot_puts(const char *str);
 extern void mm_core_init(void);
 
-extern void (*__init_array)(void);
+extern int (*__init_array)(void);
 extern spinlock_t dtor_exit_lock;
 
-void global_constructor_caller(void)
+int global_constructor_caller(void)
 {
-    void (**init_array)(void) =&__init_array;
+    int (**init_array)(void) = &__init_array;
+    int error;
 
     for (size_t i = 0; init_array[i]; i++) {
-        init_array[i]();
+        error = init_array[i]();
+        if (error) {
+            return error;
+        }
     }
 
     spin_lock_init(&dtor_exit_lock);
+
+    return 0;
 }
 
 void main(multiboot_uint8_t *mbi)
@@ -24,7 +30,10 @@ void main(multiboot_uint8_t *mbi)
     mm_core_init();
     /* do nothing */
 
-    global_constructor_caller();
+    if (global_constructor_caller() < 0) {
+        boot_puts("[x] FAILED at invoking global constructors, hlting...");
+        asm volatile ("hlt");
+    }
 
     while (1) {
         boot_puts("[x] No work todo, hlting...");
